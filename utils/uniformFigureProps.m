84c1fbf9876446e01025333c9fbf6d95c4eee49f
EmilySylwestrak <esylwestrak@gmail.com>
2022-02-27 17:24:07 -0800

    Add New Files to generate Figure 4 plots

--- utils/uniformFigureProps.m
+++ utils/uniformFigureProps.m
@@ -2,17 +2,20 @@ function  uniformFigureProps()
 %%%Make all figures uniform in size, text, and tick properties.  
 
 
+set(gcf,'DefaultAxesFontSize',7)
 %set figure size and text size
 %set(gcf, 'Position',  [378 806 360 307])
-set(gca, 'FontSize',14)
-
+set(gca, 'FontSize',7)
+set(gcf, 'PaperUnits', 'centimeters', 'Units', 'centimeters');
 %Change box and ticks
 box off
 ax = gca;
 ax.TickLength = [0.01 0.01];
 ax.TickDir = 'out';
 box off
-ax.LineWidth = 2;
-% legend('Location','northeast', 'FontSize', 10)
-% legend boxoff   
+ax.LineWidth = .75;
+ax.XRuler.TickLabelGapOffset = -2;
+ax.YRuler.TickLabelGapOffset = -1;
+
+
 end
